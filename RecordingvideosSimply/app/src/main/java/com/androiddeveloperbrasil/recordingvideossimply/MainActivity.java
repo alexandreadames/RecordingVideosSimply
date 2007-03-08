9ed94a4c6e82bb29ccd5167e63aec341eabf3b16
alexandreadames <alexandre.adames@gmail.com>
2007-03-08 01:23:46 -0300

    Gravar videos em Aplicação Android
    Aplicação exemplo de como gravar videos de forma simples em uma
    aplicação android.

---
+++ RecordingvideosSimply/app/src/main/java/com/androiddeveloperbrasil/recordingvideossimply/MainActivity.java
@@ -0,0 +1,77 @@
+package com.androiddeveloperbrasil.recordingvideossimply;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private VideoView videoView;
+    private Button btRecordaVideo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.vvVideo);
+        btRecordaVideo = (Button) findViewById(R.id.btRecordaVideo);
+
+        btRecordaVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakeVideoIntent();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = data.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.requestFocus();
+            videoView.start();
+        }
+    }
+}
